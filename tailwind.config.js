fd4693f181ee596edc1e52a9f743bd76fc9364a0
Julian Giraldo <julian09426@gmail.com>
2025-07-23 21:20:07 -0500

    template

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [],
+    theme: {
+
+        extend: {
+            backgroundImage: {
+                'bg-sofa': "url('/img/sofa.png')",
+                'bg-bed': "url('/img/frame.jpg')"
+            }
+        },
+    },
+    plugins: [],
+}
